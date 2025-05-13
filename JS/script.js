f1255fa9e2ee4a641053dc59c62409f646ca2521
FilipSpioch <biedaczeq@gmail.com>
2025-05-13 19:10:54 +0200

    commit: +style,  +JS (dodane zapisywanie wyniku do pamięci lokalnej przeglądarki)

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+const player_one_name = document.getElementsByName("imie_pl1")[0];
+const player_two_name = document.getElementsByName("imie_pl2")[0];
+
+const player_one_text = document.querySelector("#player_one_name");
+const player_two_text = document.querySelector("#player_two_name");
+
+const player_one_button = document.querySelector(".player_one");
+const player_two_button = document.querySelector(".player_two");
+const done_button = document.querySelector("#done");
+
+let p1_counter_text = document.querySelector("#p1_counter");
+let p2_counter_text = document.querySelector("#p2_counter");
+
+let p2_counter = localStorage.getItem("p2_points");
+let p1_counter = localStorage.getItem("p1_points");
+
+done_button.addEventListener("click", () => {
+  player_one_text.textContent = player_one_name.value;
+  player_two_text.textContent = player_two_name.value;
+});
+
+player_one_button.onclick = () => {
+  p1_counter++;
+  localStorage.setItem("p1_points", p1_counter);
+  p1_counter_text.textContent = p1_counter;
+};
+
+player_two_button.onclick = () => {
+  p2_counter++;
+  localStorage.setItem("p2_points", p2_counter);
+  p2_counter_text.textContent = p2_counter;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  p1_counter_text.textContent = p1_counter;
+  p2_counter_text.textContent = p2_counter;
+});
